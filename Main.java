a5a543f75913d1c38c83a4e35f60404e3586395a
afiffajar06 <afiffajarmaulana66@gmail.com>
2023-05-11 17:46:27 +0700

    UTS

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import pengguna.*;
+
 class Main {
     public static void main(String[] args) {
+
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+        mhs.setNama("Afif Fajar Maulana");
+        mhs.setNim("G.111.20.0016");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mhs:" + mhs.getNama());
+        System.out.println("Nim Mhs:" + mhs.getNim());
+        System.out.println("Smt Mhs:" + mhs.getSmt());
+       System.out.println("Username:" + mhs.getUsername());
+        System.out.println("Password:" + mhs.getPassword());
 
     }
 }
